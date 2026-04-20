1b819108c1f0b0fc347f2b7b77013b89d52295ba
Margo_Borodinowa <bmargaritaw@yandex.ru>
2026-04-20 21:10:18 +0300

    correction reports.py

--- src/reports.py
+++ src/reports.py
@@ -3,20 +3,21 @@ import logging
 import pandas as pd
 import datetime
 
+
+decor_logger = logging.getLogger(__name__)
+file_handler = logging.FileHandler("reports.log", mode="w")
+file_formater = logging.Formatter("%(asctime)s %(filename)s %(funcName)s %(levelname)s: %(message)s")
+file_handler.setFormatter(file_formater)
+decor_logger.addHandler(file_handler)
+decor_logger.setLevel(logging.INFO)
+
+
 df = pd.read_excel("../data/operations.xlsx")
 user_category = input("введите название интересущей Вас категории")
 user_date  = str(input("введите дату в формате ДД-ММ-ГГГГ"))
 
-
 def my_decorator(func):
     def wrapper(*arg, **kwargs):
-
-        decor_logger = logging.getLogger(__name__)
-        file_handler = logging.FileHandler("reports.log", mode="w")
-        file_formater = logging.Formatter("%(asctime)s %(filename)s %(funcName)s %(levelname)s: %(message)s")
-        file_handler.setFormatter(file_formater)
-        decor_logger.addHandler(file_handler)
-        decor_logger.setLevel(logging.INFO)
         decor_df = func(*arg, **kwargs)
         decor_logger.info(f"траты по заданной категории за последние три месяца{decor_df}")
         return decor_df
@@ -31,22 +32,21 @@ def spending_by_category(transactions,
 
     end_date_obj = date_obj - datetime.timedelta(days=90)
 
-    start_date = date_obj.strftime("%d.%m.%Y")
-    end_date = end_date_obj.strftime("%d.%m.%Y")
+    start_date = end_date_obj
+    end_date = date_obj
 
 
     transactions['Дата платежа'] = pd.to_datetime(transactions['Дата платежа'], format='%d.%m.%Y')
 
-    a = transactions[
-        (transactions['Дата платежа'] >= start_date) &
-        (transactions['Дата платежа'] <= end_date)
-        ]
+    mask = (transactions['Дата платежа'] >= start_date) & \
+           (transactions['Дата платежа'] <= end_date) & \
+           (transactions['Категория'] == category)
 
-    category_filter = a[a['Категория'] == 'Каршеринг']
+    category_filter = transactions[mask]
 
-    category_filter_sum = category_filter.agg({'Сумма платежа': 'sum'})
-    b = category_filter_sum.loc['Сумма платежа'].item()
-    category_filter_dict = {category:round(b,2)}
+    category_filter_sum = category_filter['Сумма платежа'].sum()
+
+    category_filter_dict = {category:round(category_filter_sum ,2)}
     json_category_filter_dict = json.dumps(category_filter_dict, ensure_ascii=False)
 
     return json_category_filter_dict
